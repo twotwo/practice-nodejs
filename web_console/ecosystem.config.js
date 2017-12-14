53f5f39775ce475120df7d5b3683839003461a49
twotwo <twotwo.li@gmail.com>
2017-12-14 15:38:10 +0800

    update ecosystem

---
+++ web_console/ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : 'web-console',
+      script    : './bin/www',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    qa : {
+      user : 'node',
+      host : '106.75.19.156',
+      ref  : 'origin/master',
+      repo : 'https://github.com/twotwo/practice-nodejs.git',
+      path : '/var/node_apps',
+      'post-deploy' : 'cd web_console && npm install && pm2 startOrRestart ecosystem.config.js --env qa',
+      env  : {
+        NODE_ENV: 'qa'
+      }
+    }
+  }
+};
