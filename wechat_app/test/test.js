00ed6f05af11d3bb5182a0f8faadf2b8037070b3
twotwo <twotwo.li@gmail.com>
2018-01-11 10:15:18 +0800

    refine test case

--- wechat_app/test/test.js
+++ wechat_app/test/test.js
@@ -19,69 +19,88 @@ var server = require('../index').server;
 // var app = 'http://106.75.19.156';
 
 // 模拟微信公共平台推送的消息
-describe('Test WeChat Service', function() {
+describe('WeChat Service', function() {
+  // shutdown wechat server after all tests
+  after(function(done) {
+    done();
 
-    after(function(done) {
-      if(typeof(server)!="undefined") {
-        console.log('shutting down the server...');
-        server.close();
-        process.exit(0);
-      }
-      done();
-    });
+    if(typeof(server)!="undefined") {
+      console.log('shutting down the server...');
+      server.close();
+      process.exit(0);
+    }
+  });
 
-  it('msgType=text', function (done) {
-    var info = {
-      sp: '李四',
-      user: '张三',
-      type: 'text',
-      text: '测试中'
-     };
-     chai.request(app)
+  describe('#msgType=text 明文消息', function() {
+
+    it('张三给李四发了一条明文信息', function (done) {
+      // var msg = '<xml><ToUserName><![CDATA[gh_d3e07d51b513]]></ToUserName><FromUserName><![CDATA[oPKu7jgOibOA-De4u8J2RuNKpZRw]]></FromUserName><CreateTime>1361374891</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[/:8-)]]></Content><MsgId>5847060634540564918</MsgId></xml>';
+      var msg = "<xml><ToUserName><![CDATA[李四]]></ToUserName>\
+          <FromUserName><![CDATA[张三]]></FromUserName>\
+          <CreateTime>1515582311579</CreateTime>\
+          <MsgType><![CDATA[text]]></MsgType>\
+          <Content><![CDATA[明文消息，你认不认？]]></Content></xml>\
+          </xml>";
+      chai.request(app)
       .post('/wechat' + tail())
-      .send(template(info))
+      .send(msg)
       .end(function(err, res) {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
         //response content
-        console.log(res.text);
+        // console.log(res.text);
         done();
       });
+    });
+
   });
 
-  it('msgType=location', function(done) { // <= Pass in done callback
-    // var msg = '<xml><ToUserName><![CDATA[gh_d3e07d51b513]]></ToUserName><FromUserName><![CDATA[oPKu7jgOibOA-De4u8J2RuNKpZRw]]></FromUserName><CreateTime>1361374891</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[/:8-)]]></Content><MsgId>5847060634540564918</MsgId></xml>';
-    var msg = "<xml><ToUserName><![CDATA[nvshen]]></ToUserName>\
-        <FromUserName><![CDATA[张三]]></FromUserName>\
-        <CreateTime>1362161914</CreateTime>\
-        <MsgType><![CDATA[location]]></MsgType>\
-        <Location_X>30.283878</Location_X>\
-        <Location_Y>120.063370</Location_Y>\
-        <Scale>15</Scale>\
-        <Label><![CDATA[]]></Label>\
-        <MsgId>5850440872586764820</MsgId>\
-        </xml>";
-    chai.request(app)
-    .post('/wechat' + tail())
-    .send(msg)
-    .end(function(err, res) {
-      expect(err).to.be.null;
-      expect(res).to.have.status(200);
-      //response content
-      // console.log(res.text);
-      done();
+  describe('#msgType=text 使用消息模板', function() {
+
+    it('张三给李四发了一条模板信息', function (done) {
+      var info = {
+        sp: 'to 李四',
+        user: 'from 张三',
+        type: 'text',
+        text: '关键字'
+      };
+      chai.request(app)
+        .post('/wechat' + tail())
+        .send(template(info))
+        .end(function(err, res) {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          //response content
+          console.log(res.text);
+          done();
+        });
     });
   });
 
-});
+  describe('#msgType=location', function() {
+    it('张三给李四发了一条位置消息', function(done) { // <= Pass in done callback
+      // var msg = '<xml><ToUserName><![CDATA[gh_d3e07d51b513]]></ToUserName><FromUserName><![CDATA[oPKu7jgOibOA-De4u8J2RuNKpZRw]]></FromUserName><CreateTime>1361374891</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[/:8-)]]></Content><MsgId>5847060634540564918</MsgId></xml>';
+      var msg = "<xml><ToUserName><![CDATA[李四]]></ToUserName>\
+          <FromUserName><![CDATA[张三]]></FromUserName>\
+          <CreateTime>1362161914</CreateTime>\
+          <MsgType><![CDATA[location]]></MsgType>\
+          <Location_X>30.283878</Location_X>\
+          <Location_Y>120.063370</Location_Y>\
+          <Scale>15</Scale>\
+          <Label><![CDATA[]]></Label>\
+          <MsgId>5850440872586764820</MsgId>\
+          </xml>";
+      chai.request(app)
+      .post('/wechat' + tail())
+      .send(msg)
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        //response content
+        // console.log(res.text);
+        done();
+      });
+    });
 
-// describe('Close Server', function(){
-//   it('send a plan text, read content', function(done) {
-//     console.log('nothing to do...');
-//   });
-//   after(function() {
-//     // runs after all tests in this block
-//     // close server
-//     app.close();
-//   });
-// });
+  });
+});
