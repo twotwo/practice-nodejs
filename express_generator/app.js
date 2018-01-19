74fc665453a5d9b44a64e864b9148b78698b3a2a
twotwo <twotwo.li@gmail.com>
2018-01-19 11:04:07 +0800

    add comment for Express

--- express_generator/app.js
+++ express_generator/app.js
@@ -1,12 +1,22 @@
+/**
+ * Gen by [express-generator](https://github.com/expressjs/generator)
+ * 
+ */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+/**
+ * [morgan](https://github.com/expressjs/morgan) HTTP request logger middleware for node.js
+ * 
+ * `common` :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]
+ * `dev` :method :url :status :response-time ms - :res[content-length]
+ */
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+/**
+ * [body-parser](https://github.com/expressjs/body-parser) parse HTTP Body to req.body
+ */
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,21 +32,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Use the express.Router class to create modular,
+ * load router modules in the app
+ */
+var index = require('./routes/index');
 app.use('/', index);
+
+var users = require('./routes/users');
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  //route to error handling
   next(err);
 });
 
-// error handler
+/**
+ * [Error handling](http://expressjs.com/en/guide/error-handling.html)
+ * 
+ */
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
