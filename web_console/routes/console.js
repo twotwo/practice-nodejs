7c1d7414544c63d139a9c100b0138592ad1fa529
twotwo <twotwo.li@gmail.com>
2018-01-23 16:44:08 +0800

    调整输入参数

--- web_console/routes/console.js
+++ web_console/routes/console.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var debug = require('debug')('console');
+
 /* Fake Info for Test */
 var info = require('../models/fake_info');
-var command = require('../models/command_helper.js');
 
-// https://nodejs.org/docs/latest-v8.x/api/child_process.html#child_process_child_process_exec_command_options_callback
-const { exec } = require('child_process');
+/* helper functions*/
+var command = require('../lib/command_helper.js');
+var expressHelper = require('../lib/express_helper.js');
 
 //https://github.com/JerrySievert/date-utils
 require('date-utils');
+
+/* Set Remote Adress in Session */
+router.use(function(req, res, next){
+	expressHelper.setRemoteAdress(req, res, next);
+});
 
 /* GET console info. */
 router.get('/', function(req, res, next) {
@@ -20,35 +27,40 @@ router.get('/', function(req, res, next) {
 
 /* POST console info. */
 router.post('/', function(req, res, next) {
-	var config = req.app.get('config');
+	if (!req.body) return res.sendStatus(400);
+
+	debug('req.body =',req.body);
 
 	var user = new Object();
+	user.channelId = req.body.channelId || '300001';
 	user.appId = req.body.appId || '';
 	user.devId = req.body.readLines || '';
-	user.ipAddress = req.body.ipAddress || '202.134.91.42';
+	user.ipAddress = req.body.ipAddress || '';
 	//读取行数
 	user.readLines = req.body.readLines || '10000';
 	//显示哪些字段
 	user.colType = req.body.colType || 'c10';
-	//日志文件地址
-	if (process.env.NODE_ENV==='prod') {
+	
+	debug('process.env.NODE_ENV = %s', process.env.NODE_ENV);
+	//获取日志文件地址
+	if (process.env.NODE_ENV==='production') {
 		var dt = new Date();
 		var todayLog = dt.toFormat("/YYYY-MM-DD.log");
-		user.logFile = config.log_dir+todayLog; //正式环境
+		user.logFile = global.config.log_dir+todayLog; //正式环境
 	}else {
-		user.logFile = config.log_dir+'/2017-12-04.log'; //非正式环境，读取本地测试文件
+		user.logFile = global.config.log_dir+'/2017-12-04.log'; //非正式环境，读取本地测试文件
 	}
-	console.log('logFile = '+user.logFile);
+	debug('logFile = '+user.logFile);
 
 	var cmd = command.genCommand(user);
 
-	exec(cmd, (error, stdout, stderr) => {
+	command.execute(cmd, function(error, stdout, stderr) {
 		if (error) {
 			//console.error(`exec error: ${error}`);
 			res.render('console', {  user: user, cmd: cmd, 
 				error: error});
 		} else {
-			console.log(`stdout: ${stdout}`);
+			// console.log(`stdout: ${stdout}`);
 			// 加载 console.hbs 模板并传递数据给模板
 			res.render('console', {  user: user, cmd: cmd, 
 				logs: command.splitLogs(user, stdout) });
