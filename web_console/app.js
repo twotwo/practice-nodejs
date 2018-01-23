56d5d01b01c2d8aca32104bbb2dcc64d0f33a669
twotwo <twotwo.li@gmail.com>
2018-01-23 16:43:28 +0800

    增加express-session

--- web_console/app.js
+++ web_console/app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+/**
+ * [express-session](https://github.com/expressjs/session)
+ */
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -40,7 +44,12 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
+app.use(session({
+  secret: "My Console",
+  resave: false,
+  saveUninitialized: false
+}));
 
 //路由设置
 app.use('/', index); //多加一个根路由
