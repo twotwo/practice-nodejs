ee6e7931c526024b1514208703fadb25e5e9b120
twotwo <twotwo.li@gmail.com>
2018-01-26 11:52:38 +0800

    refine front end js

--- wechat_app/public/js/wechat_share.js
+++ wechat_app/public/js/wechat_share.js
@@ -2,36 +2,37 @@
  * 参考： http://blog.csdn.net/lwpoor123/article/details/78678304
  */
 
-var wxfxConfig = {
-    timeStamp: '',
+var wsConfig = {
+    timestamp: '',
     nonceStr: '',
     signature: '',
 
-    zm_title: '',
-    zm_summary: '',
-    zm_link: '',
-    zm_imgurl:'',
+    ws_title: '',
+    ws_summary: '',
+    ws_link: '',
+    ws_imgurl:'',
     wxconfig: function () {
         wx.config({
-            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: wxfxConfig.appId, // 必填，公众号的唯一标识
-            timestamp: wxfxConfig.timeStamp, // 必填，生成签名的时间戳
-            nonceStr: wxfxConfig.nonceStr, // 必填，生成签名的随机串
-            signature: wxfxConfig.signature, // 必填，签名，见附录1
+            debug: wsConfig.debug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: wsConfig.appId, // 必填，公众号的唯一标识
+            timestamp: wsConfig.timestamp, // 必填，生成签名的时间戳
+            nonceStr: wsConfig.nonceStr, // 必填，生成签名的随机串
+            signature: wsConfig.signature, // 必填，签名，见附录1
             jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
         });
-        // console.log('appId = '+wxfxConfig.appId);
-        // console.log('timestamp = '+wxfxConfig.timeStamp);
-        // console.log('nonceStr = '+wxfxConfig.nonceStr);
-        // console.log('signature = '+wxfxConfig.signature);
+        console.log('debug = '+wsConfig.debug);
+        console.log('appId = '+wsConfig.appId);
+        console.log('timestamp = '+wsConfig.timestamp);
+        console.log('nonceStr = '+wsConfig.nonceStr);
+        console.log('signature = '+wsConfig.signature);
     },
     wxready: function () {
         wx.ready(function () {
             wx.onMenuShareTimeline({
-                title: wxfxConfig.zm_title,//分享标题
-                desc: wxfxConfig.zm_summary,//分享摘要
-                link: wxfxConfig.zm_link,//分享链接
-                imgUrl: wxfxConfig.zm_imgurl,//分享图片
+                title: wsConfig.ws_title,//分享标题
+                desc: wsConfig.ws_summary,//分享摘要
+                link: wsConfig.ws_link,//分享链接
+                imgUrl: wsConfig.ws_imgurl,//分享图片
                 trigger: function (res) {
                 },
                 success: function (res) {
@@ -44,10 +45,10 @@ var wxfxConfig = {
             });
 
             wx.onMenuShareAppMessage({
-                title: wxfxConfig.zm_title,
-                desc: wxfxConfig.zm_summary,
-                link: wxfxConfig.zm_link,
-                imgUrl: wxfxConfig.zm_imgurl,
+                title: wsConfig.ws_title,
+                desc: wsConfig.ws_summary,
+                link: wsConfig.ws_link,
+                imgUrl: wsConfig.ws_imgurl,
                 trigger: function (res) {
                 },
                 success: function (res) {
@@ -61,10 +62,10 @@ var wxfxConfig = {
             //新加分享到qq,微博,qq空间
 
             wx.onMenuShareQQ({
-                title: wxfxConfig.zm_title,
-                desc: wxfxConfig.zm_summary,
-                link: wxfxConfig.zm_link,
-                imgUrl: wxfxConfig.zm_imgurl,
+                title: wsConfig.ws_title,
+                desc: wsConfig.ws_summary,
+                link: wsConfig.ws_link,
+                imgUrl: wsConfig.ws_imgurl,
                 success: function (res) {
 
                 },
@@ -77,10 +78,10 @@ var wxfxConfig = {
             });
 
             wx.onMenuShareWeibo({
-                title: wxfxConfig.zm_title,
-                desc: wxfxConfig.zm_summary,
-                link: wxfxConfig.zm_link,
-                imgUrl: wxfxConfig.zm_imgurl,
+                title: wsConfig.ws_title,
+                desc: wsConfig.ws_summary,
+                link: wsConfig.ws_link,
+                imgUrl: wsConfig.ws_imgurl,
                 success: function (res) {
 
                 },
@@ -94,10 +95,10 @@ var wxfxConfig = {
 
 
             wx.onMenuShareQZone({
-                title: wxfxConfig.zm_title,
-                desc: wxfxConfig.zm_summary,
-                link: wxfxConfig.zm_link,
-                imgUrl: wxfxConfig.zm_imgurl,
+                title: wsConfig.ws_title,
+                desc: wsConfig.ws_summary,
+                link: wsConfig.ws_link,
+                imgUrl: wsConfig.ws_imgurl,
                 success: function (res) {
 
                 },
@@ -117,9 +118,9 @@ var wxfxConfig = {
     },
 
     init: function () {
-        wxfxConfig.wxconfig();
-        wxfxConfig.wxready();
-        wxfxConfig.wxerror();
+        wsConfig.wxconfig();
+        wsConfig.wxready();
+        wsConfig.wxerror();
     }
 
 }
