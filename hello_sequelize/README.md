2412af813a34613930f21cc26b7b20053cff57e3
twotwo <twotwo.li@gmail.com>
2018-02-06 11:52:30 +0800

    Sequelize 入门代码

--- hello_sequelize/README.md
+++ hello_sequelize/README.md
@@ -19,47 +19,70 @@ updated 1 package in 1.523s
 
 [mysql2](https://www.npmjs.com/package/mysql2)
 
+### conf/user.sql
+创建测试表
+
 ### conf/orm.js
 
-    "config": {"port": "3000"} #add to package.json
-
-    var port = normalizePort(process.env.npm_package_config_port||3000); #add to bin/www
+```JS
+/**
+ * Sequelize 带连接池的配置
+ */
+var config = {
+    database: 'db', // 使用哪个数据库
+    username: 'user', // 用户名
+    password: 'pass', // 口令
+    option: {
+        operatorsAliases: false, //http://docs.sequelizejs.com/manual/tutorial/querying.html#operators-security
+        host: 'host', // 主机名
+        port: 3306, // 端口号，MySQL默认3306
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 30000
+        }
+    }
+};
+module.exports = config 
+```
 
 
 ## Running Project
 
 ```bash
-git clone https://github.com/twotwo/practice-nodejs.git # or clone your own fork
-cd express_generator
 npm install
-npm start # 启动服务
-curl localhost:3000 # 访问服务
+npm run dev
 ```
 
-## Add features to Project
-
-### 1. 在首页中显示users链接
-参考 [hbs](https://github.com/pillarjs/hbs) view engine
-
-views/index.hbs
-
-```html
-<h1>{{title}}</h1>
-<p>Welcome to {{title}}</p>
-<ul>
-  <li><a href="/users/">users</a></li>
-</ul>
+```mysql
+mysql test> select * from pets;
++-----------------+--------+----------+------------+---------------+---------------+-----------+
+| id              | name   | gender   | birth      | createdAt     | updatedAt     | version   |
+|-----------------+--------+----------+------------+---------------+---------------+-----------|
+| d-1517883292806 | Odie   | 0        | 2008-08-08 | 1517883292806 | 1517883292806 | 0         |
++-----------------+--------+----------+------------+---------------+---------------+-----------+
 ```
 
-### 2. 添加一些展示用的业务数据
+## Features of Sequelize
 
-models/fake_info.js
-```js
+### 1. [Executing raw SQL queries](http://docs.sequelizejs.com/manual/installation/usage.html#executing-raw-sql-queries)
 
+```javascript
+sequelize
+  .query(
+    'SELECT * FROM projects WHERE status = :status ',
+    { raw: true, replacements: { status: 'active' } }
+  )
+  .then(projects => {
+    console.log(projects)
+  });
+
+  sequelize
+  .query('SELECT * FROM projects', { model: Projects })
+  .then(projects => {
+    // Each record will now be mapped to the project's model.
+    console.log(projects)
+  });
 ```
-
-### 3. users增删改查的业务逻辑
-
-views/user_list.hbs //展示用户列表
-routes/users.js     //业务逻辑
 
--- hello_sequelize/app.js
+++ hello_sequelize/app.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const config = require('./conf/mysql')
+const config = require('./conf/orm')
 var debug = require('debug')('sequelize');
 //配置服务
 const sequelize = new Sequelize(config.database, 
@@ -8,92 +8,133 @@ const sequelize = new Sequelize(config.database,
     config.option);
 
 //测试连接
-sequelize.authenticate()
-    .then(() => {
-      debug('Connection has been established successfully.');
-    })
-    .catch(err => {
-      debug('Unable to connect to the database:', err);
-    });
+sequelize.authenticate().then(() => {
+    debug('Connection has been established successfully.');
+}).catch(err => {
+    debug('Unable to connect to the database:', err);
+});
 
-// 映射数据库表
-var Pet = sequelize.define('pet', {
-  id: {
-    type: Sequelize.STRING(50),
-        primaryKey: true
-  },
-  name: Sequelize.STRING(100),
-    gender: Sequelize.BOOLEAN,
-    birth: Sequelize.STRING(10),
-    createdAt: Sequelize.BIGINT,
-    updatedAt: Sequelize.BIGINT,
-    version: Sequelize.BIGINT
-}, {
-  timestamps: false
+
+// define model
+const User = sequelize.define('user', {
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING
+    }
+  });
+  
+// force: true will drop the table if it already exists
+// User.sync({force: true}).then(() => {
+//     // Table created
+//     return User.create({
+//         firstName: 'John',
+//         lastName: 'Hancock'
+//     });
+// });
+
+// first query
+User.findAll().then(users => {
+  debug('findAll() acount=',users.length);
+  for(let user of users) {
+    debug('user',user.dataValues);
+  }
 })
 
-// force: true 如果表已经存在，将会丢弃表
-Pet.sync({force: true}).then(() => {
-  debug('sync done');
-});
+User.findOne().then(user => {
+    debug('firstName =',user.get('firstName'));
+  });
+  
+
+  
+
+// // 映射数据库表
+// var Pet = sequelize.define('pet', {
+//   id: {
+//     type: Sequelize.STRING(50),
+//         primaryKey: true
+//   },
+//   name: Sequelize.STRING(100),
+//     gender: Sequelize.BOOLEAN,
+//     birth: Sequelize.STRING(10),
+//     createdAt: Sequelize.BIGINT,
+//     updatedAt: Sequelize.BIGINT,
+//     version: Sequelize.BIGINT
+// }, {
+//   timestamps: false
+// })
+
+// // force: true 如果表已经存在，将会丢弃表
+// Pet.sync({force: true}).then(() => {
+//   debug('sync done');
+// });
 
 
-Pet.sync().then(() => {
-  debug('sync done');
-}).catch((ex)=>{
-  debug('failed with: '+ex);
-});
+// Pet.sync().then(() => {
+//   debug('sync done');
+// }).catch((ex)=>{
+//   debug('failed with: '+ex);
+// });
 
-var now = Date.now();
+// var now = Date.now();
 
-// 用Promise向数据库添加数据
-Pet.create({
-    id: 'g-' + now,
-    name: 'Gaffey',
-    gender: false,
-    birth: '2007-07-07',
-    createdAt: now,
-    updatedAt: now,
-    version: 0
-}).then(function (p) {
-    console.log('created.' + JSON.stringify(p));
-}).catch(function (err) {
-    console.log('failed: ' + err)
-});
+// // 用Promise向数据库添加数据
+// Pet.create({
+//     id: 'g-' + now,
+//     name: 'Gaffey',
+//     gender: false,
+//     birth: '2007-07-07',
+//     createdAt: now,
+//     updatedAt: now,
+//     version: 0
+// }).then(function (p) {
+//     console.log('created.' + JSON.stringify(p));
+// }).catch(function (err) {
+//     console.log('failed: ' + err)
+// });
 
-// 向数据库添加数据
-(async () => {
-    var dog = await Pet.create({
-        id: 'd-' + now,
-        name: 'Odie',
-        gender: false,
-        birth: '2008-08-08',
-        createdAt: now,
-        updatedAt: now,
-        version: 0
-    })
-    console.log('created: ' + JSON.stringify(dog))
-})();
+// // 向数据库添加数据
+// (async () => {
+//     var dog = await Pet.create({
+//         id: 'd-' + now,
+//         name: 'Odie',
+//         gender: false,
+//         birth: '2008-08-08',
+//         createdAt: now,
+//         updatedAt: now,
+//         version: 0
+//     });
+//     console.log('created: ' + JSON.stringify(dog));
+// })();
 
-// findAll方法查询数据库
-(async () => {
-    var pets = await Pet.findAll({
-        where: {
-            name: 'Gaffey'
-        }
-    });
-    console.log(`find ${pets.length} pets:`);
-    for (let p of pets) {
-        console.log(JSON.stringify(p));
-        console.log('update pet...');
-        p.gender = true;
-        p.updatedAt = Date.now();
-        p.version ++;
-        await p.save();
-        if (p.version === 4) {
-          console.log('lalalalalala')
-            await p.destroy();
-            console.log(`${p.name} was destroyed.`);
-        }
-    }
-})();
+// // findAll方法查询数据库
+// (async () => {
+//     var pets = await Pet.findAll({
+//         where: {
+//             name: 'Gaffey'
+//         }
+//     });
+//     console.log(`find ${pets.length} pets:`);
+//     for (let p of pets) {
+//         console.log(JSON.stringify(p));
+//         console.log('update pet...');
+//         p.gender = true;
+//         p.updatedAt = Date.now();
+//         p.version ++;
+//         await p.save();
+//         if (p.version === 4) {
+//           console.log('lalalalalala')
+//             await p.destroy();
+//             console.log(`${p.name} was destroyed.`);
+//         }
+//     }
+// })();
+
+//close
+// debug('close connection...');
+// sequelize.close().then(() => {
+//     debug('Connection has been closed successfully.');
+// }).catch(err => {
+//     debug('Unable to close the database:', err);
+// });
