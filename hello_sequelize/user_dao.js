7521f529b0b304206e8082839625881a2dc0df62
twotwo <twotwo.li@gmail.com>
2018-02-07 19:10:39 +0800

    四种获取dao model的示例

--- hello_sequelize/user_dao.js
+++ hello_sequelize/user_dao.js
@@ -1,3 +1,8 @@
+/**
+ * 参考 [Model definition](http://docs.sequelizejs.com/manual/tutorial/models-definition.html)
+ * 
+ * 本代码演示了获得DAO(Model)的三种方法
+ */
 const debug = require('debug')('sequelize:dao:user');
 
 const Factory = require('./libs/sequelize_factory');
@@ -6,9 +11,10 @@ const sequelize = Factory.sequelize;
 debug('sequelize.pool = %O', sequelize.options.pool);
 debug('sequelize.define = %O', sequelize.options.define);
 
-const Users = ['张三', '李四', '王二', '陈武', '赵六'];
-
-const user_dao = sequelize.import('user', (sequelize, DataTypes) => {
+/**
+ * 1. 直接调用sequelize.define
+ */
+function get_dao_define() {
   return sequelize.define('user', {
     username: {
       type: Sequelize.STRING(100)
@@ -19,8 +25,45 @@ const user_dao = sequelize.import('user', (sequelize, DataTypes) => {
     email: {
       type: Sequelize.STRING(100)
     }
-  }, {timestamps: false})
-});
+  }, {timestamps: false});
+};
+
+/**
+ * 2. 调用sequelize.define再包一层sequelize.import
+ */
+function get_dao_import() {
+  return sequelize.import('user', (sequelize, DataTypes) => {
+    debug('get_dao_import', 'cache model');
+    return sequelize.define('user', {
+      username: {
+        type: Sequelize.STRING(100)
+      },
+      password: {
+        type: Sequelize.STRING(16)
+      },
+      email: {
+        type: Sequelize.STRING(100)
+      }
+    }, {timestamps: false})
+  });
+};
+
+const Users = ['张三', '李四', '王二', '陈武', '赵六'];
+
+
+/**
+ * 3. 第二种方法的变体，model写到.js中
+ */
+let user_dao = sequelize.import('user', require('./models/user'));
+
+/**
+ * 4. 再进一步，包到工厂方法中
+ */
+// user_dao = Factory.getDataAcessObject('user');
+
+// user_dao = get_dao_define();
+
+// user_dao = get_dao_import();
 
 // force: true will drop the table if it already exists
 // try {
@@ -36,10 +79,7 @@ const user_dao = sequelize.import('user', (sequelize, DataTypes) => {
 //   debug('sync(false) table');
 // }
 
-debug('user_dao = %O', user_dao);
-sequelize.query("SELECT count(*) FROM users;").then(myTableRows => {
-  debug('myTableRows', myTableRows);
-})
+
 
 // first query
 user_dao.findAll().then(users => {
@@ -58,10 +98,23 @@ user_dao.findAll().then(users => {
 user_dao.findAll().then(users => {
   debug('findAll() count=',users.length);
   for(let user of users) {
-    debug('user',user.dataValues);
+    debug('user',user.username);
   }
 })
 
 user_dao.findOne().then(user => {
+  if(user) {
     debug('username =',user.get('username'));
+  }
+});
+
+//sync and close pool connections
+sequelize.sync().then(() => {
+  debug("handles before:", process._getActiveHandles().length);
+  return sequelize.close().then(() => {
+    debug("handles after:", process._getActiveHandles().length);
+  })
+})
+.then(() => {
+  debug('shutdown gracefully', process._getActiveHandles().length);
 });
