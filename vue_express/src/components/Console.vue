cc14ed0507bac9dc696bf8d880b38107fb2f20d6
twotwo <twotwo.li@gmail.com>
2018-03-30 20:38:51 +0800

    前端逻辑优化

--- vue_express/src/components/Console.vue
+++ vue_express/src/components/Console.vue
@@ -1,78 +1,114 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
     <h2><a href="/">Index</a></h2>
-
-    <form action="/console" method="post">
+  
+    <form id="head" action="/console" method="post">
       <!-- text -->
-      <p>应用ID(AppID) <input type="text" name="appId" v-model="appId"> </p>
-      <p>渠道ID(ChannelID) <input type="text" name="channelId" v-model="channelId"> </p>
-      <p>手机信息(IMEI/IDFA) <input type="text" name="devId" v-model="devId"> </p>
-      <p>设备标识(UID) <input type="text" name="uid" v-model="uid"> </p>
-      <p>IP地址 <input type="text" name="ipAddress" v-model="ipAddress"> </p>
+      <p>应用ID(AppID) <input type="text" name="appId" v-model="head.appId"> </p>
+      <p>渠道ID(ChannelID) <input type="text" name="channelId" v-model="head.channelId"> </p>
+      <p>手机信息(IMEI/IDFA) <input type="text" name="devId" v-model="head.devId"> </p>
+      <p>设备标识(UID) <input type="text" name="uid" v-model="head.uid"> </p>
+      <p>IP地址 <input type="text" name="ipAddress" v-model="head.ipAddress"> </p>
       <p>data: {{ $data }}</p>
       <input type="submit" name="submit" value="Submit">
+      <p v-on:click="submit()">查询</p>
     </form>
-
-
+  
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Console',
-  data () {
-    return {
-      msg: 'Console',
-      appId      : '',
-      channelId  : '300001',
-      devId  : '',
-      uid   : '',
-      ipAddress: '',
-      columns: 'c17',
-    }
+  import {
+    doGet,
+    doPost
+  } from "../util/axios"
+  
+  let head = {
+    appId: '',
+    channelId: '300001',
+    devId: '',
+    uid: '',
+    ipAddress: '',
+    columns: 'c17',
   }
-}
+  
+  export default {
+    name: 'Console',
+    data() {
+      return {
+        msg: 'Console',
+        head: head,
+        body: {}
+      }
+    },
+    methods: {
+      details() {
+        console.log('details() ' + this.$data);
+      },
+      submit() {
+        console.log('submit() ...');
+        let query = '';
+        doPost('/console/query', this.$data.head)
+          .then((resp) => {
+            query = resp.data;
+            console.log(query);
+          })
+          .catch((err) => {
+            console.log(err)
+          });
+        console.log('query = ');
+        console.log(query);
+      }
+    }
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-body {
-  padding: 50px;
-  font: 14px "Lucida Grande", Helvetica, Arial, sans-serif;
-}
-
-a {
-  color: #00B7FF;
-}
-
-.rTable {
-  display: table;
-  width: 100%;
-}
-.rTableRow {
-  display: table-row;
-}
-.rTableHeading {
-  display: table-header-group;
-  background-color: #ddd;
-}
-.rTableCell, .rTableHead {
-  display: table-cell;
-  padding: 3px 10px;
-  border: 1px solid #999999;
-}
-.rTableHeading {
-  display: table-header-group;
-  background-color: #ddd;
-  font-weight: bold;
-}
-.rTableFoot {
-  display: table-footer-group;
-  font-weight: bold;
-  background-color: #ddd;
-}
-.rTableBody {
-  display: table-row-group;
-}
+  body {
+    padding: 50px;
+    font: 14px "Lucida Grande", Helvetica, Arial, sans-serif;
+  }
+  
+  a {
+    color: #00B7FF;
+  }
+  
+  .rTable {
+    display: table;
+    width: 100%;
+  }
+  
+  .rTableRow {
+    display: table-row;
+  }
+  
+  .rTableHeading {
+    display: table-header-group;
+    background-color: #ddd;
+  }
+  
+  .rTableCell,
+  .rTableHead {
+    display: table-cell;
+    padding: 3px 10px;
+    border: 1px solid #999999;
+  }
+  
+  .rTableHeading {
+    display: table-header-group;
+    background-color: #ddd;
+    font-weight: bold;
+  }
+  
+  .rTableFoot {
+    display: table-footer-group;
+    font-weight: bold;
+    background-color: #ddd;
+  }
+  
+  .rTableBody {
+    display: table-row-group;
+  }
 </style>
--- vue_express/src/components/Index.vue
+++ vue_express/src/components/Index.vue
@@ -1,39 +1,43 @@
 <template>
-  <div class="hello">
+  <div>
     <img src="../assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    
+  
     <h2>Console</h2>
-      <a href="#/Console">Console</a>
+    <a href="#/Console">Console</a>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Index',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'Index',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
 </style>
