4fba27a740033be02488fd634d8d6361b756f81b
twotwo <twotwo.li@gmail.com>
2018-06-11 19:02:38 +0800

    1. lib 版本升级 2. sequelize集成到业务类中 1） 回调方法来获得数据 2） 直接返回Promise对象

--- hello_sequelize/services/user.js
+++ hello_sequelize/services/user.js
@@ -1,47 +1,61 @@
 //获取Sequelize单一实例
-const Factory = require('./libs/sequelize_factory');
-
+const Factory = require("../libs/sequelize_factory")
 // define model
-const user_dao = Factory.getDataAcessObject('user');
-
-User () => {
-
-}
-
-User.prototype.findAll =  (() => {
-  user_dao.findAll().then(users => {
-    return (users)
-  })
-  return []
-})
-
-User.prototype.findOne = ( username => {
+const user_dao = Factory.getDataAcessObject("user")
+const User = {
+  Factory,
   user_dao
-    .findOne({ where: { username: username } })
-    .then(user => {
-      return user
+}
+const debug = require("debug")("service:user")
+
+// User.findAll = () => {
+//   return user_dao.findAll()
+
+// }
+
+/**
+ *
+ * @param {*} callback (err, users)
+ */
+User.findAll = callback => {
+  user_dao
+    .findAll()
+    .then(users => {
+      return callback(null, users)
     })
     .catch(err => {
-      debug("user_dao.findOne failed: username[%s], err=%O", username, err);
-    });
-    return null
-})
+      return callback(err, null)
+    })
+}
 
-User.prototype.update = ((user, id) => {
+/**
+ *
+ * @param {*} username
+ */
+User.findByUsername = username => {
+  return user_dao.findOne({ where: { username: username } })
+  // return new Promise((resolve, reject) => {
+  //   user_dao
+  //     .findOne({ where: { username: username } })
+  //     .then(user => {
+  //       resolve(user)
+  //     })
+  //     .catch(err => {
+  //       debug("User.findOne failed: username[%s], err=%O", username, err)
+  //     })
+  // })
+}
+
+User.update = (user, id) => {
   user_dao
-    .update(user,
-      {
-        where: {
-          id: id
-        }
-      })
+    .update(user, { where: { id: id } })
     .then(user => {
       return true
     })
     .catch(err => {
-      debug("user_dao.update failed: username[%s], err=%O", username, err);
+      debug("user_dao.update failed: username[%s], err=%O", username, err)
       return false
-    });
-})
+    })
+}
 
 module.exports = User
--- hello_sequelize/test/user_service.test.js
+++ hello_sequelize/test/user_service.test.js
@@ -4,22 +4,50 @@
  * 测试用户服务对象 User
  */
 process.env.NODE_ENV = "dev"
-const debug = require("debug")("service:user")
-// Sequelize, sequelize 全局唯一实例
-const User = require("../services/user")
+const debug = require("debug")("service:user:test")
+
+let userService = require("../services/user")
 
 const query_key = 888888
 
 // 测试用例
 describe("基于 User 表的业务逻辑", () => {
-
+  beforeAll(() => {
+    debug("beforeAll")
+  })
+  // shutdown after all tests
+  afterAll(() => {
+    let Factory = require("../libs/sequelize_factory")
+    // debug('sequelize.pool = %O', Factory.sequelize.connectionManager.pool);
+    Factory.sequelize.close()
+    debug("afterAll")
+  })
   describe("[管理员]查看用户信息", () => {
-    test("#order_dao.add", () => {
-      let users = User.findAll()
-      debug('find %d users', users.length)
-      for(let user in users) {
-        console.log('user.username =', user.username)
-      }
+    test("#userService.findAll", done => {
+      userService.findAll((err, users) => {
+        if (err) {
+          debug("userService.findAll err = %O", err)
+          return
+        }
+        debug("find %d users", users.length)
+        for (let i = 0; i < users.length; i++) {
+          debug("user.username = %s", users[i].username)
+        }
+        done()
+      })
     })
 
+    test("#userService.findByUsername", done => {
+      // let user = userService.findOne("李四")
+      // expect(user.username).toBe("李四")
+      // debug("user = %O", user)
+
+      userService.findByUsername("李四").then(user => {
+        debug("user = %O", user.dataValues)
+        // expect(err).toBeNull()
+        expect(user.username).toEqual("李四")
+        done()
+      })
+    })
+  })
 })
