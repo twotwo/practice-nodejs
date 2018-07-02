2c7dcb57a771763b9975940a469e8e24c70c05bf
twotwo <twotwo.li@gmail.com>
2018-07-02 18:14:22 +0800

    用户名和email增加唯一性约束: KEY -> UNIQUE KEY   UNIQUE KEY `idx_username` (`username`) ,   UNIQUE KEY `idx_email` (`email`)

--- hello_sequelize/models/user.sql
+++ hello_sequelize/models/user.sql
@@ -6,8 +6,8 @@ CREATE TABLE `t_project_user` (
   `signin_time` int(10) DEFAULT 0 COMMENT '用户最新签到时间',
   `score` int(5) DEFAULT 0 COMMENT '用户总积分',
   PRIMARY KEY (`id`),
-  KEY `idx_username` (`username`) ,
-  KEY `idx_email` (`email`)
+  UNIQUE KEY `idx_username` (`username`) ,
+  UNIQUE KEY `idx_email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `t_project_user` (username, password, email) VALUES ('张三','pass','张三@m.com'),('李四','pass','李四@m.com'),('王二','pass','王二@m.com'),('陈武','pass','陈武@m.com');
