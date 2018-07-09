b8de7369d1a960e11df5b409187da33d956f85c0
twotwo <twotwo.li@gmail.com>
2018-07-09 18:09:05 +0800

    add debug info

--- express_generator/routes/users.js
+++ express_generator/routes/users.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const router = express.Router()
 
+const debug = require("debug")("srv:api:user")
+
 /* Fake Info for Demo */
 const info = require("../models/fake_info")
 
-/* GET users listing. */
+/**
+ * users list
+ * 
+ */
 router.get("/", function(req, res, next) {
   console.log("/api/users ", req.baseUrl)
   res.send({ title: "List Users", users: info.users })
