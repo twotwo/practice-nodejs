97a8a2be031b42cb2241523feb470add72116156
twotwo <twotwo.li@gmail.com>
2018-07-31 15:18:54 +0800

    add response cache

---
+++ express_generator/libs/express_helper.js
@@ -0,0 +1,72 @@
+let debug = require("debug")("helper:express")
+
+/**
+ * set remote address in session
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+exports.setRemoteAdress = (req, res, next) => {
+  // expose session to *.hbs
+  res.locals.session = req.session
+  if (!req.session.remoteAdress) {
+    req.session.remoteAdress = (
+      req.headers["x-forwarded-for"] ||
+      // req.ip ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress
+    ).split(",")[0]
+  }
+  next()
+}
+
+/**
+ * 获取应用运行配置
+ */
+exports.getConfig = () => {
+  let config = {}
+  if (process.env.NODE_ENV === "dev") {
+    if (process.platform === "win32") {
+      config = require("../config/dev.env.js") // windows开发环境的配置
+      debug("load ../conf/dev.env.js")
+    } else {
+      config = require("../config/dev.env.js") // 非windows的开发环境
+      debug("load ../conf/dev.env.js")
+    }
+  } else if (process.env.NODE_ENV === "qa") {
+    config = require("../config/test.env.js") // 测试环境的配置
+    debug("load ../conf/test.env.js")
+  } else {
+    config = require("../config/prod.env.js") // 正式环境的配置
+    debug("load ../conf/prod.env.js")
+  }
+  return config
+}
+
+// https://www.npmjs.com/package/memory-cache
+const cache = require("memory-cache")
+
+/**
+ * 
+ * @param {*} duration store cache in seconds
+ */
+exports.cache = duration => {
+  return (req, res, next) => {
+    let key = "__express__" + req.originalUrl || req.url
+    let cachedBody = cache.get(key)
+    if (cachedBody) {
+      res.send(cachedBody)
+      return
+    } else {
+      debug('saving key[%s]', key)
+      res.sendResponse = res.send
+      res.send = body => {
+        cache.put(key, body, duration * 1000)
+        res.sendResponse(body)
+      }
+      next()
+    }
+  }
+}
--- express_generator/routes/users.js
+++ express_generator/routes/users.js
@@ -6,11 +6,13 @@ const debug = require("debug")("srv:route:user")
 /* Fake Info for Demo */
 const info = require("../models/fake_info")
 
+const cache = require("../libs/express_helper").cache
+
 /**
  * users list
  *
  */
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   debug("/a/users ", req.baseUrl)
   res.send({ title: "List Users", users: info.users })
 })
@@ -30,7 +32,7 @@ const delay = (t, v) => {
 /**
  * ab -c 10 -n 10 "http://localhost:3000/users/perf/100"
  */
-router.get("/perf/:time", function(req, res, next) {
+router.get("/perf/:time", cache(10), (req, res, next) => {
   debug("/api/users ", req.baseUrl)
   let time = req.params.time || 1000
   if (isNaN(time)) {
@@ -45,7 +47,7 @@ router.get("/perf/:time", function(req, res, next) {
   // }, time)
   return delay(time).then(() => {
     debug("process %d ms ...", time)
-    res.send("perf over\n")
+    res.send({ title: 'Hey', message: 'Hello there', date: new Date() })
     res.end()
   })
 })
