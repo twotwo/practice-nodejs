52848a8b159caae7ba4b236f236b5a8d76edd902
twotwo <twotwo.li@gmail.com>
2018-08-29 18:45:50 +0800

    learn jest

--- jest-on-express/test/jest.test.js
+++ jest-on-express/test/jest.test.js
@@ -95,5 +95,121 @@ describe('Jest Usage', () => {
         expect(z).toBeFalsy()
       })
     })
+
+    /**
+     * [Numbers](https://jestjs.io/docs/en/using-matchers#numbers)
+     *
+     */
+    describe('Numbers', () => {
+      test('two plus two', () => {
+        const value = 2 + 2
+        expect(value).toBeGreaterThan(3)
+        expect(value).toBeGreaterThanOrEqual(3.5)
+        expect(value).toBeLessThan(5)
+        expect(value).toBeLessThanOrEqual(4.5)
+
+        // toBe and toEqual are equivalent for numbers
+        expect(value).toBe(4)
+        expect(value).toEqual(4)
+      })
+
+      test('adding floating point numbers', () => {
+        const value = 0.1 + 0.2
+        // expect(value).toBe(0.3);           This won't work because of rounding error
+        expect(value).toBeCloseTo(0.3) // This works.
+      })
+    })
+
+    /**
+     * [Strings](https://jestjs.io/docs/en/using-matchers#strings)
+     *
+     */
+    describe('Strings', () => {
+      test('there is no I in team', () => {
+        expect('team').not.toMatch(/I/)
+      })
+
+      test('but there is a "stop" in Christoph', () => {
+        expect('Christoph').toMatch(/stop/)
+      })
+    })
+
+    /**
+     * [Arrays](https://jestjs.io/docs/en/using-matchers#arrays)
+     *
+     */
+    describe('Arrays', () => {
+      const shoppingList = [
+        'diapers',
+        'kleenex',
+        'trash bags',
+        'paper towels',
+        'beer'
+      ]
+
+      test('the shopping list has beer on it', () => {
+        expect(shoppingList).toContain('beer')
+      })
+    })
+
+    /**
+     * [Exceptions](https://jestjs.io/docs/en/using-matchers#exceptions)
+     *
+     */
+    describe('Exceptions', () => {
+      function compileAndroidCode () {
+        throw new Error('you are using the wrong JDK')
+      }
+
+      test('compiling android goes as expected', () => {
+        expect(compileAndroidCode).toThrow()
+        expect(compileAndroidCode).toThrow(Error)
+
+        // You can also use the exact error message or a regexp
+        expect(compileAndroidCode).toThrow('you are using the wrong JDK')
+        expect(compileAndroidCode).toThrow(/JDK/)
+      })
+    })
+
+    describe('Object', () => {
+      const can1 = {
+        flavor: 'grapefruit',
+        ounces: 12
+      }
+      const can2 = {
+        flavor: 'grapefruit',
+        ounces: 12
+      }
+
+      describe('the La Croix cans on my desk', () => {
+        test('have all the same properties', () => {
+          expect(can1).toEqual(can2)
+        })
+        test('are not the exact same can', () => {
+          expect(can1).not.toBe(can2)
+        })
+      })
+
+      const houseForSale = {
+        bath: true,
+        bedrooms: 4,
+        kitchen: {
+          amenities: ['oven', 'stove', 'washer'],
+          area: 20,
+          wallColor: 'white'
+        }
+      }
+      const desiredHouse = {
+        bath: true,
+        kitchen: {
+          amenities: ['oven', 'stove', 'washer'],
+          wallColor: expect.stringMatching(/white|yellow/)
+        }
+      }
+
+      test('the house has my desired features', () => {
+        expect(houseForSale).toMatchObject(desiredHouse)
+      })
+    })
   })
 })
